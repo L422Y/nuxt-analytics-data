849e63f897f2f74235f7f17091aa95ed24bc4476
Larry W <l422y@l422y.com>
2023-03-14 16:18:15 -0400

    fix(deploy): use defineLazyEventHandler

--- src/runtime/server/api/analyticsData.get.ts
+++ src/runtime/server/api/analyticsData.get.ts
@@ -5,46 +5,50 @@ import { useRuntimeConfig } from "#imports"
 import memoryDriver from "unstorage/drivers/memory"
 import { createStorage } from "unstorage"
 
-const storage = createStorage({
-  driver: memoryDriver(),
-})
+export default defineLazyEventHandler(async () => {
 
-storage.setItemRaw("cache:analyticsData", null)
-storage.setItemRaw("cache:analyticsDataCacheProcessing", false)
-storage.setItemRaw("cache:analyticsDataCacheRefresh", false)
+  const storage = createStorage({
+    driver: memoryDriver(),
+  })
 
-export default defineEventHandler(async (event) => {
-  let trendingCache = ( await storage.getItemRaw("cache:analyticsData") )
-  const trendingCacheProcessing = ( await storage.getItemRaw("cache:analyticsDataCacheProcessing") ) as boolean
-  const trendingCacheRefresh = ( await storage.getItemRaw("cache:analyticsDataCacheRefresh") ) as boolean
+  await storage.setItemRaw("cache:analyticsData", null)
+  await storage.setItemRaw("cache:analyticsDataCacheProcessing", false)
+  await storage.setItemRaw("cache:analyticsDataCacheRefresh", false)
 
-  appendHeader(event, "Access-Control-Allow-Origin", "*")
-  appendHeader(event, "Content-Type", "application/json")
+  return defineEventHandler(async (event) => {
+    let trendingCache = ( await storage.getItemRaw("cache:analyticsData") )
+    const trendingCacheProcessing = ( await storage.getItemRaw("cache:analyticsDataCacheProcessing") ) as boolean
+    const trendingCacheRefresh = ( await storage.getItemRaw("cache:analyticsDataCacheRefresh") ) as boolean
 
-  const query: QueryObject = getQuery(event)
-  let path: string = query.path as string
-  const config = await useRuntimeConfig()
-  if (!config.analyticsData.exact && path != "/") {
-    path = path.replace(/\/$/, "")
-  }
+    appendHeader(event, "Access-Control-Allow-Origin", "*")
+    appendHeader(event, "Content-Type", "application/json")
 
-  const shouldRefresh = !trendingCacheProcessing && ( trendingCache === null || trendingCacheRefresh )
-
-  if (shouldRefresh) {
-    trendingCache = await useGoogleAnalyticsDataReports(config, trendingCache)
-    await storage.setItemRaw("cache:analyticsData", trendingCache)
-    setTimeout(async () => {
-      await storage.setItem("cache:analyticsDataCacheRefresh", true)
-    }, config.analyticsData.cacheTimeout * 1000)
-  }
-
-  if (trendingCache != null) {
-    if (path === "*") {
-      return trendingCache
-    } else if (path in trendingCache) {
-      // @ts-ignore
-      return {views: parseInt(trendingCache[path])}
+    const query: QueryObject = getQuery(event)
+    let path: string = query.path as string
+    const config = await useRuntimeConfig()
+    if (!config.analyticsData.exact && path != "/") {
+      path = path.replace(/\/$/, "")
     }
-  }
-  return {views: 0, empty: true}
+
+    const shouldRefresh = !trendingCacheProcessing && ( trendingCache === null || trendingCacheRefresh )
+
+    if (shouldRefresh) {
+      trendingCache = await useGoogleAnalyticsDataReports(config, trendingCache)
+      await storage.setItemRaw("cache:analyticsData", trendingCache)
+      setTimeout(async () => {
+        await storage.setItem("cache:analyticsDataCacheRefresh", true)
+      }, config.analyticsData.cacheTimeout * 1000)
+    }
+
+    if (trendingCache != null) {
+      if (path === "*") {
+        return trendingCache
+      } else if (path in trendingCache) {
+        // @ts-ignore
+        return {views: parseInt(trendingCache[path])}
+      }
+    }
+    return {views: 0, empty: true}
+  })
+
 })
